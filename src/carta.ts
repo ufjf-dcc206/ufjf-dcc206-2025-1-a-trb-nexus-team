0744f1bfc1b96a3203701b5645d6e96623991442
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-07-26 10:48:55 -0300

    implementação do getPeso

--- src/carta.ts
+++ src/carta.ts
@@ -1,10 +1,34 @@
+//criando types personalizados para Naipe e Valor
 export type Naipe = '♠' | '♥' | '♦' | '♣';
 export type Valor = 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
 
+// criação da classe Carta e seus métodos
 class Carta{
-  constructor(naipe:string, valor:string){
+ // atributos padrões da classe em forma privada
+  private naipe: Naipe;
+  private valor: Valor;
+  constructor(naipe:Naipe, valor:Valor){
     this.naipe = naipe;
     this.valor = valor;
+  }
+
+  getPeso():number{
+    switch(this.valor){
+      case 'A': return 15;
+      case '2': return 2;
+      case '3': return 3;
+      case '4': return 4;
+      case '5': return 5;
+      case '6': return 6;
+      case '7': return 7;
+      case '8': return 8;
+      case '9': return 9;
+      case '10': return 10;
+      case 'J': return 10;
+      case 'Q': return 10;
+      case 'K': return 10;
+    }
+    throw new Error('Valor inválido');
   }
 
 }
